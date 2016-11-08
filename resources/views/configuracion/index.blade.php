cbaedeeaec9e7428cde91d8daebf69de538af790
ismalizarraga4 <ismalizarraga4@gmail.com>
2016-11-08 15:58:33 -0700

    Update index.blade.php

--- resources/views/configuracion/index.blade.php
+++ resources/views/configuracion/index.blade.php
@@ -4,12 +4,14 @@
 @section('content')
 	<table class="table">
 		<thead>
-			<th>Nombre</th>
-			<th>Correo</th>
+			<th>Tasa Financiamiento</th>
+			<th>Enganche</th>
+			<th>Plazo Maximo</th>
 			<th>Operacion</th>
 		</thead>
 		@foreach($configuraciones as $configuracion)
 		<tbody>
+			<td>{{$configuracion->tasa_financiamiento}}</td>
 			<td>{{$configuracion->enganche}}</td>
 			<td>{{$configuracion->plazo_max}}</td>
 			<td>
